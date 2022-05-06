dc66e08bc8f97b1fe052d34a3abf7b99b0b2e8ec
Sophia Mcgeown <sopmcgeo@WKMZT3A84AC1.global.publicisgroupe.net>
2022-05-06 16:12:10 +0100

    Added package.json

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bcrypt = require("bcrypt");
 const { User, JournalEntry } = require("./db");
@@ -7,6 +8,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 
 //Create user - signup
 app.post("/users", async (req, res) => {
