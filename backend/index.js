c67647462e018590da4a946aea77560547bd6da3
Sophia Mcgeown <sopmcgeo@WKMZT3A84AC1.global.publicisgroupe.net>
2022-05-12 12:52:54 +0100

    Revert "Update index.js"
    This reverts commit c7b00441b5c13fea753adb33ce35ecf34538d32e.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const app = require("./app");
 const { sequelize, User } = require("./db");
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log("listening on", port));
+app.listen(4000, () => console.log("listening on", 4000));
