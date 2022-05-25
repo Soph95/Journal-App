5da9a9e1823f3450cf0dacff1b9d0fa2a275a72b
Sophia Mcgeown <sopmcgeo@WKMZT3A84AC1.global.publicisgroupe.net>
2022-05-25 17:13:53 +0100

    Revert "Update index.js"
    This reverts commit 66304a68c1168375c3ad790ffb415bfbc85a227b.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const app = require("./app");
 const { sequelize, User } = require("./db");
 
-// const { Pool } = require("pg");
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 app.listen(process.env.PORT || 4000, () =>
   console.log("Server started successfully")
