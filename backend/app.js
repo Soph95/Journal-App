82a0bbabc4a3d274060918af08da3654784e3a6d
Sophia Mcgeown <sopmcgeo@WKMZT3A84AC1.global.publicisgroupe.net>
2022-05-25 16:53:22 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,18 +13,18 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 
 //DB
-app.get("/db", async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT * FROM test_table");
-    const results = { results: result ? result.rows : null };
-    res.render("pages/db", results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
+// app.get("/db", async (req, res) => {
+//   try {
+//     const client = await pool.connect();
+//     const result = await client.query("SELECT * FROM test_table");
+//     const results = { results: result ? result.rows : null };
+//     res.render("pages/db", results);
+//     client.release();
+//   } catch (err) {
+//     console.error(err);
+//     res.send("Error " + err);
+//   }
+// });
 
 //Create user - signup
 app.post("/users", async (req, res) => {
