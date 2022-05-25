135f3e7ed7114478abd0871b77ea949b598f92cb
Sophia Mcgeown <sopmcgeo@WKMZT3A84AC1.global.publicisgroupe.net>
2022-05-25 16:20:08 +0100

    Update app.js & db.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,8 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 
 //DB
 app.get("/db", async (req, res) => {
--- backend/db.js
+++ backend/db.js
@@ -6,11 +6,33 @@ const location =
     ? ":memory:"
     : path.join(__dirname, "db.sqlite");
 
-const sequelize = new Sequelize({
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: location,
+//   logging: process.env.NODE_ENV !== "test",
+// });
+
+let settings = {
   dialect: "sqlite",
   storage: location,
   logging: process.env.NODE_ENV !== "test",
-});
+};
+
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  settings.dialect = "postgres";
+  settings.ssl = true;
+  settings.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  };
+  sequelize = new Sequelize(process.env.DATABASE_URL, settings);
+} else {
+  sequelize = new Sequelize(settings);
+}
 
 class User extends Model {}
 
