7337e6755a3a232f47a58712997bd9b93d1bf470
Sophia Mcgeown <sopmcgeo@WKMZT3A84AC1.global.publicisgroupe.net>
2022-05-25 11:25:48 +0100

    Add Procfile

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
 const app = require("./app");
 const { sequelize, User } = require("./db");
 
-app.listen(4000, () => console.log("listening on", 4000));
+app.listen(process.env.PORT || 4000, () => console.log("listening on", 4000));
